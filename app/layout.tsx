d806979584a6aa4540b080bb93ee1b7d0f8b0779
Keshav Kumar <94639526+DeveloperKeshavKumar@users.noreply.github.com>
2025-08-01 22:24:12 +0530

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata, Viewport } from "next";
+import { Analytics } from "@vercel/analytics/next"
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider"
@@ -49,6 +50,7 @@ export default function RootLayout({
           </main>
           <Footer />
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
